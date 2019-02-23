0d3b16730db2fab7d101e2f6ed8b4b8c550b5750
Amos Garner <aagarner.7@gmail.com>
2019-02-23 10:07:07 -0500

    Refactor app module spacing

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { NotesComponent } from "./components/notes/notes.component";
-import { HttpClientModule } from "@angular/common/http";
 import { HeadingComponent } from "./components/heading/heading.component";
-import { HeaderComponent } from './components/header/header.component';
-import { NoteComponent } from './components/note/note.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { NoteComponent } from "./components/note/note.component";
 
 @NgModule({
   declarations: [
