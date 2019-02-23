e20044bed5466d862d77c25e9611b60c4b69c554
Amos Garner <aagarner.7@gmail.com>
2019-02-22 22:39:16 -0500

    Install cors middleware and implement accept all cors

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,14 +1,15 @@
-const bodyParser = require('body-parser');
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const notesController = require("../controllers/notesController");
 
 module.exports = app => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
-  app.route("/api/v1/notes")
-    .get(notesController.list);
-  app.route("/api/v1/note")
+  app.use(cors());
+  
+  app.route("/api/v1/notes").get(notesController.list);
+  app
+    .route("/api/v1/note")
     .get(notesController.get)
     .post(notesController.create)
     .put(notesController.update)
