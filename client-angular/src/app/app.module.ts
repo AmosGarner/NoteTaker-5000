129d8e7d27fcc999d4875411de1677d5a5a3cddc
Amos Garner <aagarner.7@gmail.com>
2019-02-22 23:36:12 -0500

    Replace app html w/ call to heading component

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { NotesComponent } from "./components/notes/notes.component";
 import { HttpClientModule } from "@angular/common/http";
+import { HeadingComponent } from './components/heading/heading.component';
 
 @NgModule({
-  declarations: [AppComponent, NotesComponent],
+  declarations: [AppComponent, NotesComponent, HeadingComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
