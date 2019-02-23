761ae2af637a27ce322cd0d6250585d97b0ccaa2
Amos Garner <aagarner.7@gmail.com>
2019-02-23 15:33:04 -0500

    Implement create note functionality on frontend.

--- client-angular/src/app/components/notes/notes.component.ts
+++ client-angular/src/app/components/notes/notes.component.ts
@@ -1,27 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Note } from 'src/app/models/note';
-import { NoteService } from 'src/app/services/note.service';
+import { Component, OnInit } from "@angular/core";
+import { Note } from "src/app/models/note";
+import { NoteService } from "src/app/services/note.service";
 
 @Component({
-  selector: 'app-notes',
-  templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.css']
+  selector: "app-notes",
+  templateUrl: "./notes.component.html",
+  styleUrls: ["./notes.component.css"]
 })
 export class NotesComponent implements OnInit {
-  notes: Note[];
-  
-  constructor(public notesService: NoteService) { }
+  private notes: Note[];
+
+  constructor(public noteService: NoteService) {}
   ngOnInit() {
     this.getNotes();
   }
 
-  getNotes(): void{
-    this.notesService.getNotes()
-      .subscribe(notes => this.notes = notes);
+  getNotes(): void {
+    this.noteService.list().subscribe(notes => (this.notes = notes));
   }
-
-  createNote(): void{
-    console.log("Create a new Note!");
+  
+  createNote(title: string, body: string): void {
+    title = title.trim();
+    body = body.trim();
+    if (!title || !body) return;
+    this.noteService.create({ title, body } as Note)
+      .subscribe(note => this.notes.push(note));
   }
-
 }
--- client-angular/src/app/services/note.service.ts
+++ client-angular/src/app/services/note.service.ts
@@ -1,18 +1,40 @@
 import { Injectable } from '@angular/core';
 import { Note } from '../models/note';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class NoteService {
-  private notesURL = 'http://localhost:3000/api/v1/notes';
+  private apiURL = 'http://localhost:3000/api/v1';
+  
   constructor(private http: HttpClient) { }
 
-  getNotes(): Observable<Note[]>{
-    return this.http.get<Note[]>(this.notesURL);
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
+  list(): Observable<Note[]>{
+    return this.http.get<Note[]>(`${this.apiURL}/notes/`)
+    .pipe(
+      catchError(this.handleError('list', []))
+    );
+  }
+
+  create(note: Note): any {
+    return this.http.post<Note>(`${this.apiURL}/note`, note, httpOptions).pipe(
+      tap((newNote: Note) => console.log('Note Created:', newNote)),
+      catchError(this.handleError<Note>('create'))
+    )
   }
 }
