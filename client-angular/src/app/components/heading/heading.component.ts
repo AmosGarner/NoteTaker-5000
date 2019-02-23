96612705c196e3e5a06a6a9dc524ed943c586e9b
Amos Garner <aagarner.7@gmail.com>
2019-02-22 23:35:43 -0500

    Implement heading component

---
+++ client-angular/src/app/components/heading/heading.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-heading",
+  templateUrl: "./heading.component.html",
+  styleUrls: ["./heading.component.css"]
+})
+export class HeadingComponent {
+  @Input() strength: number;
+  @Input() text: string;
+  @Input() classes: string;
+}
